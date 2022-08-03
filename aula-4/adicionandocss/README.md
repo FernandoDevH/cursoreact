901e271b93ff54badaddfc01f513b8744159cb09
Fernando Henrique <fernandohsilva@outlook.com.br>
2022-08-03 17:06:34 -0300

    Aula 4
    Criação da aula

---
+++ aula-4/adicionandocss/README.md
@@ -0,0 +1,3 @@
+# Adicionando CSS
+- O CSS pode ser adicinado de forma globla na aplicação por meio do arquivo index.css por exemplo, 
+porem é possível estiliza a nível de componentes utilizando o css modules para isso. Basta criar uma arquivo como: **nomedocomponente.module.css** e chamamr este CSS no componente
---
+++ aula-4/adicionandocss/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Pessoa from './components/Pessoa'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Pessoa nome="fernando"/>
+      </div>
+    );
+  }
+}
+
+export default App;
