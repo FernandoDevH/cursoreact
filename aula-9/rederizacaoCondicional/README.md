5b55a3c6b5cb34b31b1552b17e102a669e91269c
FernandoDevH <fernandohsilva@outlook.com.br>
2022-08-30 16:28:03 -0300

    Aula 9

---
+++ aula-9/rederizacaoCondicional/README.md
@@ -0,0 +1,3 @@
+# Renderização por condição
+
+- Podemos atrelar a exibição de algum elemento a um if, esta ação é chamada de renderização condicional. Envolvemos as tags em chaves {} e como as chaves executam o JavaScript, criamos nossa condição. É possível usar o state para criar as condições. 
---
+++ aula-9/rederizacaoCondicional/src/components/Form.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useState } from 'react'
+
+function Form() {
+    function cadastrarUsuario(e) {
+        e.preventDefault()
+        console.log (`Usuário ${name} foi cadastrado com a senha: ${password}`)
+       
+    }
+    const [name, setName]=useState("")
+    const [password, setPassword]=useState()
+
+    return (
+        <div>
+            <h1>Meu Cadastro:</h1>
+            <form onSubmit={cadastrarUsuario}>
+                <div>
+                    <label htmlFor="name">Nome: </label>
+                    <input type="text"
+                        id="name"
+                        name="name"
+                        placeholder="Digite seu nome aqui"
+                        onChange={(e) => setName(e.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="password">Senha: </label>
+                    <input type="password" 
+                    id="password" 
+                    name="password" 
+                    placeholder="Digite sua senha aqui"
+                    onChange={(e) => setPassword(e.target.value)}  />
+                </div>
+                <div>
+                    <input type="submit" value="Cadastrar" />
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Form
